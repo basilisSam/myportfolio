9aea1cf20f90dedcb42691aa2a6c5f3e0ff5c828
Basilis Samoladas <33910381+basilisSam@users.noreply.github.com>
2021-11-24 22:52:35 +0000

    update style

--- src/componets/HeroSection/HeroElements.js
+++ src/componets/HeroSection/HeroElements.js
@@ -6,7 +6,7 @@ export const HeroContainer = styled.div`
   justify-content: center;
   align-items: center;
   padding: 0 30px;
-  height: 1000px;
+  height: 100%;
   position: relative;
   z-index: 1;
 `;
@@ -38,6 +38,7 @@ export const HeroContent = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  margin-bottom: 50%;
 `;
 
 export const HeroH1 = styled.h1`
